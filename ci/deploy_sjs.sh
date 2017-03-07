9a8b49905b21a3a4dfd25987da560d318ebf1f0b
Noorul Islam K M <noorul@noorul.com>
2017-03-07 20:43:53 +0530

    Debug

--- ci/deploy_sjs.sh
+++ ci/deploy_sjs.sh
@@ -3,5 +3,5 @@
 docker build -t sjsclient_ft -f ci/Dockerfile.test .
 docker run --name sjs_db -d sjsclient_ft /bin/bash -c "/opt/h2/bin/h2-server.sh;"
 docker ps -a
-docker run --link sjs_db:db --name sjs -d sjsclient_ft /bin/bash -c "cd /opt/spark-jobserver; ./start.sh;"
+docker run --link sjs_db:db --name sjs sjsclient_ft /bin/bash -c "cd /opt/spark-jobserver; ./start.sh;"
 docker ps -a
